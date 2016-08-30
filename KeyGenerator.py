518e69798fe98e3efaf1d14432856f29cd40099e
davsanba <xQE80us9uy1>
2016-08-29 23:12:53 -0500

    commit de antes de dormir

--- KeyGenerator.py
+++ KeyGenerator.py
@@ -1,5 +1,18 @@
+import random
+import sys
+
+
 class KeyGenerator:
     def __init__(self, textLen):
+        self.len = textLen
         self.size = (textLen^2)/4
 
     def generator(self):
+        rnd = random.SystemRandom()
+        return rnd.randint(0, self.len), rnd.randint(0, self.len)
+
+    
+
+new = KeyGenerator(4)
+
+print new.generator()
